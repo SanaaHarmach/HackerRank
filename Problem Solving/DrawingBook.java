5bef5222610cd62568fc3d6c9a42b4a79f3296e5
harmach <54673833+SanaaHarmach@users.noreply.github.com>
2020-11-20 21:12:35 +0100

    Update DrawingBook.java

--- Problem Solving/DrawingBook.java
+++ Problem Solving/DrawingBook.java
@@ -59,7 +59,7 @@ public class Solution {
     static int pageCount(int n, int p) {
         int totalPageTurnCountFromFront = n/2;
         int targetPageTurnCountFromFront = p/2;
-        int targetPageTurnCountFromBack = totalPageTurnCountFromFront -                                                            targetPageTurnCountFromFront;
+        int targetPageTurnCountFromBack = totalPageTurnCountFromFront -  targetPageTurnCountFromFront;
     return Math.min(targetPageTurnCountFromFront, targetPageTurnCountFromBack);
     }
 
